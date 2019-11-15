4db20ef01bcd93777b805b39785c29030f3af428
unknown <17110217@student.hcmute.edu.vn>
2019-11-15 13:28:27 +0700

    crud

--- Shop2/Areas/Admin/Controllers/LoaiMatHangController.cs
+++ Shop2/Areas/Admin/Controllers/LoaiMatHangController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Hosting.Internal;
 using Shop2.Data;
+using Shop2.Models;
 using Shop2.Models.ViewModel;
+using Shop2.Utility;
 
 namespace Shop2.Areas.Admin.Controllers
 {
@@ -33,6 +36,14 @@ namespace Shop2.Areas.Admin.Controllers
             var loaiMatHang = _db.LoaiMatHangs.Include(m => m.GianHang);
             return View(await loaiMatHang.ToListAsync());
         }
+        public IActionResult Create()
+        {
+            return View(LoaiMatHangVM);
+        }
+
+        //Post : Products Create
+        [HttpPost, ActionName("Create")]
+        [ValidateAntiForgeryToken]
         public async Task<IActionResult> CreatePOST()
         {
             if (!ModelState.IsValid)
@@ -40,10 +51,139 @@ namespace Shop2.Areas.Admin.Controllers
                 return View(LoaiMatHangVM);
             }
 
-            _db.LoaiMatHangs.Add(LoaiMatHangVM.Products);
+            _db.LoaiMatHangs.Add(LoaiMatHangVM.LoaiMatHang);
             await _db.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
 
+        }
+
+        //GET : Edit
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            LoaiMatHangVM.LoaiMatHang = await _db.LoaiMatHangs.Include(m => m.GianHang).SingleOrDefaultAsync(m => m.MatHangId == id);
+
+            if (LoaiMatHangVM.LoaiMatHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(LoaiMatHangVM);
+        }
+
+
+        //Post : Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id)
+        {
+            if (ModelState.IsValid)
+            {
+              //  string webRootPath = _hostingEnvironment.WebRootPath;
+               // var files = HttpContext.Request.Form.Files;
+
+                var LMHFromDb = _db.LoaiMatHangs.Where(m => m.MatHangId == LoaiMatHangVM.LoaiMatHang.MatHangId).FirstOrDefault();
+
+                //if (files.Count > 0 && files[0] != null)
+                //{
+                //    //if user uploads a new image
+                //    var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                //    var extension_new = Path.GetExtension(files[0].FileName);
+                //    var extension_old = Path.GetExtension(productFromDb.Image);
+
+                //    if (System.IO.File.Exists(Path.Combine(uploads, ProductsVM.Products.Id + extension_old)))
+                //    {
+                //        System.IO.File.Delete(Path.Combine(uploads, ProductsVM.Products.Id + extension_old));
+                //    }
+                //    using (var filestream = new FileStream(Path.Combine(uploads, ProductsVM.Products.Id + extension_new), FileMode.Create))
+                //    {
+                //        files[0].CopyTo(filestream);
+                //    }
+                //    ProductsVM.Products.Image = @"\" + SD.ImageFolder + @"\" + ProductsVM.Products.Id + extension_new;
+                //}
+
+                //if (ProductsVM.Products.Image != null)
+                //{
+                //    productFromDb.Image = ProductsVM.Products.Image;
+                //}
+
+                LMHFromDb.TenMatHang = LoaiMatHangVM.LoaiMatHang.TenMatHang;
+                LMHFromDb.MoTaMatHang = LoaiMatHangVM.LoaiMatHang.MoTaMatHang;
+                LMHFromDb.GianHangId = LoaiMatHangVM.LoaiMatHang.GianHangId;
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(LoaiMatHangVM);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            LoaiMatHangVM.LoaiMatHang = await _db.LoaiMatHangs.Include(m => m.GianHang).SingleOrDefaultAsync(m => m.MatHangId == id);
+
+            if (LoaiMatHangVM.LoaiMatHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(LoaiMatHangVM);
+        }
+
+        //POST : Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+          //  string webRootPath = _hostingEnvironment.WebRootPath;
+            LoaiMatHang loaiMatHang = await _db.LoaiMatHangs.FindAsync(id);
+
+            if (loaiMatHang == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                //var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                //var extension = Path.GetExtension(products.Image);
+
+                //if (System.IO.File.Exists(Path.Combine(uploads, products.Id + extension)))
+                //{
+                //    System.IO.File.Delete(Path.Combine(uploads, products.Id + extension));
+                //}
+                _db.LoaiMatHangs.Remove(loaiMatHang);
+                await _db.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        //GET : Details
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            LoaiMatHangVM.LoaiMatHang = await _db.LoaiMatHangs.Include(m => m.GianHang).SingleOrDefaultAsync(m => m.MatHangId == id);
+
+            if (LoaiMatHangVM.LoaiMatHang == null)
+            {
+                return NotFound();
+            }
+
+            return View(LoaiMatHangVM);
         }
 
 
--- Shop2/Areas/Admin/Controllers/SanPhamController.cs
+++ Shop2/Areas/Admin/Controllers/SanPhamController.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting.Internal;
 using Shop2.Data;
 using Shop2.Models;
+using Shop2.Utility;
 
 namespace Shop2.Areas.Admin.Controllers
 {
@@ -12,10 +16,14 @@ namespace Shop2.Areas.Admin.Controllers
     public class SanPhamController : Controller
     {
         private readonly ApplicationDbContext _db;
+        private readonly IHostingEnvironment _hostingEnvironment;
+        
 
-        public SanPhamController(ApplicationDbContext db)
+        public SanPhamController(ApplicationDbContext db, IHostingEnvironment hostingEnvironment)
         {
             _db = db;
+            _hostingEnvironment = hostingEnvironment;
+
         }
         public IActionResult Index()
         {
@@ -26,18 +34,63 @@ namespace Shop2.Areas.Admin.Controllers
             return View();
         }
 
-        [HttpPost]
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public async Task<IActionResult> Create(SanPham sanPham)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _db.Add(sanPham);
+        //        await _db.SaveChangesAsync();
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    return View(sanPham);
+        //}
+
+        //Post : Products Create
+        [HttpPost, ActionName("Create")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(SanPham sanPham)
+        public async Task<IActionResult> CreatePOST(SanPham sanPham)
         {
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                _db.Add(sanPham);
-                await _db.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return View(sanPham);
             }
-            
-            return View(sanPham);
+
+            _db.Add(sanPham);
+            await _db.SaveChangesAsync();
+
+            //Image being saved
+
+            string webRootPath = _hostingEnvironment.WebRootPath;
+            var files = HttpContext.Request.Form.Files;
+
+            var productsFromDb = _db.SanPhams.Find(sanPham.SanPhamId);
+
+            if (files.Count != 0)
+            {
+                //Image has been uploaded
+                var uploads = Path.Combine(webRootPath, SD.ImageFolder);
+                var extension = Path.GetExtension(files[0].FileName);
+
+                using (var filestream = new FileStream(Path.Combine(uploads, sanPham.SanPhamId + extension), FileMode.Create))
+                {
+                    files[0].CopyTo(filestream);
+                }
+                productsFromDb.Image = @"\" + SD.ImageFolder + @"\" + sanPham.SanPhamId + extension;
+            }
+            else
+            {
+                //when user does not upload image
+                var uploads = Path.Combine(webRootPath, SD.ImageFolder + @"\" + SD.DefaultProductImage);
+                System.IO.File.Copy(uploads, webRootPath + @"\" + SD.ImageFolder + @"\" + sanPham.SanPhamId + ".png");
+                productsFromDb.Image = @"\" + SD.ImageFolder + @"\" + sanPham.SanPhamId + ".png";
+            }
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
         }
         public async Task<IActionResult> Edit(int? id)
         {
